42b656bd968dfee6042f9ce7230fcfb1a76e3789
Ali Irawan <boylevantz@gmail.com>
2019-01-03 13:36:23 +0700

    adding SimpleTest make sure phpunit is working

---
+++ tests/SimpleTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ali Irawan (boylevantz@gmail.com)
+ * Date: 1/3/19
+ * Time: 1:33 PM
+ */
+
+namespace Xtra\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+
+    public function testNothing() {
+
+        $this->assertEquals(1,1);
+        
+    }
+}
